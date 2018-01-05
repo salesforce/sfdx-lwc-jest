de46bd45c690d64823c8a70635cb671a6fa699d0
Trevor <tbliss@salesforce.com>
2018-01-04 20:43:28 -0800

    Use log utility

--- src/commands/test.js
+++ src/commands/test.js
@@ -3,9 +3,8 @@ const path = require('path');
 const chalk = require('chalk');
 const jest = require('jest');
 
-const {
-    PROJECT_ROOT,
-} = require('../utils/project.js');
+const { error } = require('../log.js');
+const { PROJECT_ROOT } = require('../utils/project.js');
 
 function applyOverrides(config) {
     const jestConfig = path.resolve(PROJECT_ROOT, 'jest.config.js');
@@ -25,21 +24,18 @@ function applyOverrides(config) {
         });
         const unsupportedKeys = Object.keys(overrides);
         if (unsupportedKeys.length) {
-            console.error(
-                chalk.red(
-                    'Out of the box, only the following Jest optiosn are supported:\n\n' +
-                    supportedKeys.map(key => chalk.bold('  \u2022 ' + key)).join('\n') +
-                    '.\n\n' +
-                    'These options in your package.json Jest configuration ' +
-                    'are not currently supported:\n\n' +
-                    unsupportedKeys
-                        .map(key => chalk.bold('  \u2022 ' + key))
-                        .join('\n') +
-                    '\n\nIf you wish to override other Jest options, please file ' +
-                    'an issue to discuss supporting more options out of the box.\n'
-                )
+            error(
+                'Out of the box, only the following Jest optiosn are supported:\n\n' +
+                supportedKeys.map(key => chalk.bold('  \u2022 ' + key)).join('\n') +
+                '.\n\n' +
+                'These options in your package.json Jest configuration ' +
+                'are not currently supported:\n\n' +
+                unsupportedKeys
+                    .map(key => chalk.bold('  \u2022 ' + key))
+                    .join('\n') +
+                '\n\nIf you wish to override other Jest options, please file ' +
+                'an issue to discuss supporting more options out of the box.\n'
             );
-            process.exit(1);
         }
     }
     return config;
@@ -59,7 +55,11 @@ module.exports = {
     },
 
     async handler(argv) {
-        // TODO: validate in project root here (by checking existence of sfdx-project.json)? 
+        const cwd = fs.realpathSync(process.cwd());
+        if (!fs.existsSync(path.join(cwd, 'sfdx-project.json'))) {
+            error('Could not find file sfdx-project.json in cwd, please run lts-test from SFDX project root.');
+        }
+
         let config = {
             moduleFileExtensions: ['js'],
             transform: {
