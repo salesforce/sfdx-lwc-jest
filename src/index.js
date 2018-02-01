26e84da962daf0f2895cb24402728260ea0098e7
Trevor <tbliss@salesforce.com>
2018-02-01 10:58:14 -0800

    Better error message

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@
 
 const yargs = require('yargs');
 
-const { error } = require('./log');
+const { 
+    error,
+    info,
+} = require('./log');
 const test = require('./commands/test');
 
 const isAdvanced = yargs.argv.advancedMode;
@@ -11,6 +14,27 @@ yargs
     .command(test)
     .demandCommand()
     .strict(!isAdvanced)
+    .fail((msg, err, yargs) => {
+        if (err) {
+            throw err;
+        }
+
+        let unrecognized;
+        let token = 'Unknown argument: ';
+        if (msg.startsWith(token)) {
+            unrecognized = msg.substring(token.length);
+        }
+        token = 'Unknown arguments: ';
+        if (msg.startsWith(token)) {
+            unrecognized = msg.substring(token.length);
+        }
+
+        let template = `The following argument(s) are not recognized by lts-jest: ${unrecognized}\n` +
+        `If you wish to pass these arguments along to Jest, please add the --advancedMode flag`;
+
+        info(yargs.help());
+        error(template);
+    })
     .help()
     .argv;
 
