d0c0a0782956a52eca687c6836c5448d7e07b15b
Trevor <tbliss@salesforce.com>
2018-08-08 14:22:45 -0700

    add formatStyle prop to lightning-formatted-number

--- src/lightning-mocks/lightning-formatted-number/lightning-formatted-number.js
+++ src/lightning-mocks/lightning-formatted-number/lightning-formatted-number.js
@@ -1,6 +1,7 @@
 import { Element, api } from 'engine';
 
 export default class FormattedNumber extends Element {
+	@api formatStyle
     @api currencyCode
 	@api currencyDisplayAs
 	@api maximumFractionDigits
