318435947a8229f67a7582f7773b171bec17cbc0
Trevor <tbliss@salesforce.com>
2018-12-07 10:26:02 -0800

    add LICENSE and license headers

--- src/utils/module.js
+++ src/utils/module.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
 const path = require('path');
 
 const PATH_TO_MODULES = path.join('main', 'default', 'lwc');
