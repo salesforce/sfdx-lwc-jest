24727ed707c48334fbc5a5f8ee58d7e51df13a6c
Trevor <tbliss@salesforce.com>
2018-12-10 16:49:24 -0800

    CI script to check license headers (#4)
    * CI script to check license headers
    
    * remove nexus urls

--- scripts/upgrade-lwc.sh
+++ scripts/upgrade-lwc.sh
@@ -1,5 +1,10 @@
 #/bin/bash -e
 
+# Copyright (c) 2018, salesforce.com, inc.
+# All rights reserved.
+# SPDX-License-Identifier: MIT
+# For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+
 LWC_VERSION=$1
 
 yarn upgrade @lwc/compiler@$LWC_VERSION
