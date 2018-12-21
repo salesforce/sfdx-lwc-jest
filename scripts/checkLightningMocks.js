4890684e6eae707e61410b9b591287d686f0ba01
Trevor <tbliss@salesforce.com>
2018-12-20 18:29:19 -0800

    Add missing mocks, update script to catch more (#9)
    * add missing mocks, update script
    
    * add missing mocks

--- scripts/checkLightningMocks.js
+++ scripts/checkLightningMocks.js
@@ -17,12 +17,23 @@ const path = require('path');
 
 const pathToMocks = path.join(__dirname, '..', 'src', 'lightning-mocks');
 const lightningNamespaces = [ 'lightning', 'interop' ];
+/*
+ * some components have multiple html files in the bundle and dynamically
+ * which to load in the render() method. Map to the most standard or default
+ * html to get the slot count.
+ */
+const dynamicHtmlMapper = {
+    'tile': 'standardTile',
+    'progressStep': 'base',
+}
 const pathToComponents = process.argv[2];
 
 function checkIsExposed(bundleDir, file) {
     const metaXmlPath = path.join(bundleDir, file + '.js-meta.xml');
     const metaXml = fs.existsSync(metaXmlPath) && fs.readFileSync(metaXmlPath, 'utf8');
-    return metaXml && metaXml.indexOf('<isExposed>true</isExposed>') !== -1;
+    return metaXml
+        && metaXml.indexOf('<isExposed>true</isExposed>') !== -1
+        && metaXml.indexOf('</minApiVersion>') !== -1;
 }
 
 function getSlotCount(htmlPath) {
@@ -33,57 +44,73 @@ function getSlotCount(htmlPath) {
     return slotCount;
 }
 
+function getHtmlPath(base, fileName) {
+    // we don't use the dynamic html files in the mocks
+    if (base.indexOf('lightning-mocks') === -1 && dynamicHtmlMapper[fileName]) {
+        fileName = dynamicHtmlMapper[fileName];
+    }
+    return path.join(base, fileName + '.html');
+}
+
 if (!pathToComponents) {
     console.log('Error: Missing path to lightning modules directory. Usage: "node checkLightningMocks.js ~/path/to/lightning/modules"');
     process.exit(1);
 }
 
-// exposed lightning components with an html file and at least one <slot> present in markup
-let matches = [];
+let exposed = [];
 lightningNamespaces.forEach(ns => {
     const nsDir = path.join(pathToComponents, ns);
     fs.readdirSync(nsDir).forEach(file => {
         const bundleDir = path.join(nsDir, file);
-        const htmlPath = path.join(bundleDir, file + '.html');
-        const hasHtml = fs.existsSync(htmlPath);
         const isExposed = checkIsExposed(bundleDir, file);
-        if (hasHtml && isExposed) {
-            const slots = getSlotCount(htmlPath);
-            if (slots > 0) {
-                matches.push({ file, slots });
-            }
+
+        if (!isExposed) {
+            return;
         }
+
+        const htmlPath = getHtmlPath(bundleDir, file);
+        const hasHtml = fs.existsSync(htmlPath);
+        const slotCount = hasHtml && getSlotCount(htmlPath) || 0;
+        exposed.push({
+            file,
+            slotCount,
+        });
     });
 });
 
 let missingMocks = [];
 let missingSlots = [];
-matches.forEach(entry => {
+exposed.forEach(entry => {
     const file = entry.file;
-    const bundleDir = path.join(pathToMocks, file);
+    const mocksDir = path.join(pathToMocks, file);
 
-    if (!fs.existsSync(bundleDir)) {
+    if (!fs.existsSync(mocksDir)) {
         missingMocks.push(file);
         return;
     }
 
-    const htmlPath = path.join(bundleDir, file + '.html');
-    const slots = getSlotCount(htmlPath);
-    if (slots !== entry.slots) {
+    const htmlPath = getHtmlPath(mocksDir, file);
+    const slotCount = getSlotCount(htmlPath);
+    if (slotCount !== entry.slotCount) {
         missingSlots.push({
             file,
-            expectedSlotCount: entry.slots,
-            actualSlotCount: slots,
+            expectedSlotCount: entry.slotCount,
+            actualSlotCount: slotCount,
         });
     }
+
 });
 
+let exitCode = 0;
 if (missingMocks.length > 0) {
     console.log('Missing lightning mocks: ', missingMocks);
-    process.exit(1);
+    exitCode = 1;
 }
 
 if (missingSlots.length > 0) {
     console.log('Missing slots on the following mocks: ', missingSlots);
-    process.exit(1);
+    exitCode = 1;
 }
+
+console.log('Exiting with exit code ', exitCode);
+process.exit(exitCode);
---
+++ src/lightning-mocks/datatableKeyboardMixins/datatableKeyboardMixins.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+export const baseNavigation = superclass => {
+    return class extends superclass {
+        @api keyboardMode
+        @api focus
+        [updateActionables]() {}
+        [handleArrowKeyDown]() {}
+        [handleArrowLeft]() {}
+        [handleArrowRight]() {}
+        [handleTabKey]() {}
+        [getActiveElement]() {}
+        [moveToPreviousOf]() {}
+        [moveToNextOf]() {}
+    };
+};
