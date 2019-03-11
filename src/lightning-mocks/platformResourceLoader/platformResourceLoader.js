720e43eb60f82202c7a4cfb67ecc2473aef732c8
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2019-03-11 05:33:25 +0100

    Defaulting platformResourceLoader mocks as promise (#38)

--- src/lightning-mocks/platformResourceLoader/platformResourceLoader.js
+++ src/lightning-mocks/platformResourceLoader/platformResourceLoader.js
@@ -4,5 +4,5 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-export const loadStyle = jest.fn();
-export const loadScript = jest.fn();
+export const loadStyle = jest.fn().mockResolvedValue();
+export const loadScript = jest.fn().mockResolvedValue();
