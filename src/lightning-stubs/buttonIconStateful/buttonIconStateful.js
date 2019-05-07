9e927ed0725b173c4af3b2cb94ecf2ee9b7bd334
Trevor <tbliss@salesforce.com>
2019-05-07 11:10:24 -0700

    add disable prop to buttons (#54)

--- src/lightning-stubs/buttonIconStateful/buttonIconStateful.js
+++ src/lightning-stubs/buttonIconStateful/buttonIconStateful.js
@@ -8,6 +8,7 @@ import { LightningElement, api } from 'lwc';
 
 export default class ButtonIconStateful extends LightningElement {
     @api alternativeText
+    @api disabled
     @api iconName
     @api name
     @api selected
