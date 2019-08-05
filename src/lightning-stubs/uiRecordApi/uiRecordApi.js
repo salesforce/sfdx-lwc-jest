ba93f5ee70ed797ff48a87916b4fdaea2cc3bd58
Trevor <tbliss@salesforce.com>
2019-08-05 10:33:11 -0700

    use real impl

--- src/lightning-stubs/uiRecordApi/uiRecordApi.js
+++ src/lightning-stubs/uiRecordApi/uiRecordApi.js
@@ -16,12 +16,50 @@ export const getRecordInput = jest.fn();
 export const refresh = jest.fn().mockResolvedValue();
 export const getRecordUi = jest.fn();
 export const getFieldValue = jest.fn((record, field) => {
-    if (record) {
-        const fields = field.fieldApiName.split('.');
-        const fieldData = fields.reduce((o, i) => o[i], record.fields);
-        if (fieldData && fieldData.value) {
-            return fieldData.value;
+    const unqualifiedField = splitQualifiedFieldApiName(getFieldApiName(field))[1];
+    const fields = unqualifiedField.split(".");
+    let r = record;
+    while (fields.length > 0 && r && r.fields) {
+        const f = fields.shift();
+        const fvr = r.fields[f];
+        if (fvr === undefined) {
+            return undefined;
+        }
+        else {
+            r = fvr.value;
         }
     }
+    return r;
 });
 export const getFieldDisplayValue = jest.fn();
+
+/**
+ * Returns the field API name, qualified with an object name if possible.
+ * @param value The value from which to get the qualified field API name.
+ * @return The qualified field API name.
+ */
+function getFieldApiName(value) {
+    if (typeof value === "string") {
+        return value;
+    }
+    else if (value && typeof value.objectApiName === "string" && typeof value.fieldApiName === "string") {
+        return value.objectApiName + "." + value.fieldApiName;
+    }
+    throw new TypeError("Value is not a string or FieldId.");
+}
+
+/**
+ * Split the object API name and field API name from a qualified field name.
+ * Eg: Opportunity.Title returns ['Opportunity', 'Title']
+ * Eg: Opportunity.Account.Name returns ['Opportunity', 'Account.Name']
+ * @param fieldApiName The qualified field name.
+ * @return The object and field API names.
+ */
+function splitQualifiedFieldApiName(fieldApiName) {
+    const idx = fieldApiName.indexOf(".");
+    if (idx < 1) {
+        // object api name must non-empty
+        throw new TypeError("Value does not include an object API name.");
+    }
+    return [fieldApiName.substring(0, idx), fieldApiName.substring(idx + 1)];
+}
