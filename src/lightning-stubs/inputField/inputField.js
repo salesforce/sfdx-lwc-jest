73c411363b040194d5c729fe3f5e1091eb36cff4
Jennifer Bennett <jennifer.bennett@salesforce.com>
2019-10-29 16:16:03 -0500

    Add missing required property - prettier format

--- src/lightning-stubs/inputField/inputField.js
+++ src/lightning-stubs/inputField/inputField.js
@@ -4,15 +4,16 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class InputField extends LightningElement {
-    @api dirty
-    @api disabled
-    @api fieldName
-    @api readonly
-    @api value
-    @api variant
+    @api dirty;
+    @api disabled;
+    @api fieldName;
+    @api readonly;
+    @api required;
+    @api value;
+    @api variant;
     @api reportValidity() {}
     @api reset() {}
     @api setErrors() {}
