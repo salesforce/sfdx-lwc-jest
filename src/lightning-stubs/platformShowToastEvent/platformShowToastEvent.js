ae792597b19aeb0b951a419eba1a534e468f1c5e
Kevin C. Gall <kcappieg@users.noreply.github.com>
2019-10-31 14:38:14 -0400

    Proposed change - make toasts usable in tests
    Bringing file inline with lwc-recipe:
    https://github.com/salesforce/sfdx-lwc-jest/blob/master/src/lightning-stubs/platformShowToastEvent/platformShowToastEvent.js

--- src/lightning-stubs/platformShowToastEvent/platformShowToastEvent.js
+++ src/lightning-stubs/platformShowToastEvent/platformShowToastEvent.js
@@ -4,14 +4,15 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-const ShowToastEventName = 'lightning__showtoast';
+export const ShowToastEventName = 'lightning__showtoast';
 
 export class ShowToastEvent extends CustomEvent {
-    constructor() {
+    constructor(toast) {
         super(ShowToastEventName, {
             composed: true,
             cancelable: true,
             bubbles: true,
+            detail: toast
         });
     }
 }
