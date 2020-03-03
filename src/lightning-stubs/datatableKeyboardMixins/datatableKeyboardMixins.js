ed9cdb08792bca3b6888cd8df963208c9928221f
Pierre-Marie Dartus <p.dartus@salesforce.com>
2020-03-03 13:40:13 +0100

    chore: Enable linting on all the lightning stubs

--- src/lightning-stubs/datatableKeyboardMixins/datatableKeyboardMixins.js
+++ src/lightning-stubs/datatableKeyboardMixins/datatableKeyboardMixins.js
@@ -4,6 +4,17 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+import { api } from 'lwc';
+
+const updateActionables = Symbol('updateActionables');
+const handleArrowKeyDown = Symbol('handleArrowKeyDown');
+const handleArrowLeft = Symbol('handleArrowLeft');
+const handleArrowRight = Symbol('handleArrowRight');
+const handleTabKey = Symbol('handleTabKey');
+const getActiveElement = Symbol('getActiveElement');
+const moveToPreviousOf = Symbol('moveToPreviousOf');
+const moveToNextOf = Symbol('moveToNextOf');
+
 export const baseNavigation = superclass => {
     return class extends superclass {
         @api keyboardMode;
--- src/lightning-stubs/navigation/navigation.js
+++ src/lightning-stubs/navigation/navigation.js
@@ -10,8 +10,8 @@ const Navigate = Symbol('Navigate');
 const GenerateUrl = Symbol('GenerateUrl');
 export const NavigationMixin = Base => {
     return class extends Base {
-        [Navigate](pageReference, replace) {}
-        [GenerateUrl](pageReference) {}
+        [Navigate]() {}
+        [GenerateUrl]() {}
     };
 };
 NavigationMixin.Navigate = Navigate;
