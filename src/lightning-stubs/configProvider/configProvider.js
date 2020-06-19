a39853ecfda37975066121e2c20663b3ed799ebe
Trevor <tbliss@salesforce.com>
2020-06-18 20:43:03 -0700

    Update stubs for Summer 20 (#150)
    * update stubs for Summer 20
    
    * APPLICATION_SCOPE as symbol

--- src/lightning-stubs/configProvider/configProvider.js
+++ src/lightning-stubs/configProvider/configProvider.js
@@ -7,8 +7,6 @@
 export default jest.fn();
 export const getPathPrefix = jest.fn();
 export const getToken = jest.fn();
-export const getLocale = jest.fn();
 export const getLocalizationService = jest.fn();
-export const getCoreInfo = jest.fn();
 export const getIconSvgTemplates = jest.fn();
 export const getOneConfig = jest.fn();
