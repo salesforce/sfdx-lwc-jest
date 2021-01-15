83ffc6df7e707b1684d041721e40a876c34f9f05
Trevor <tbliss@salesforce.com>
2021-01-15 08:47:16 -0800

    Add README note about --skipApiVersionCheck flag (#190)
    * Add README note about --skipApiVersionCheck flag
    
    * fix typo

--- src/options/options.js
+++ src/options/options.js
@@ -39,7 +39,8 @@ const testOptions = {
     },
 
     skipApiVersionCheck: {
-        description: 'Disable the "sourceApiVersion" field check before running tests',
+        description:
+            'Disable the "sourceApiVersion" field check before running tests. **Warning** By disabling this check you risk running tests against stale versions of the framework. See details here: https://github.com/salesforce/sfdx-lwc-jest#disabling-the-sourceApiVersion-check',
         type: 'boolean',
         default: false,
     },
