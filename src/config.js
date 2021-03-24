fc065b0e6774660c3a7262720a190703efe86148
Ravi Jayaramappa <ravi.jayaramappa@salesforce.com>
2021-03-24 10:37:27 -0700

    chore: prepare for summer21 release (#205)
    * chore: prepare for summer21 release
    
    updated other dependencies as well
    
    * chore: update husky to v5

--- src/config.js
+++ src/config.js
@@ -35,6 +35,6 @@ const jestConfig = {
     snapshotSerializers: [require.resolve('@lwc/jest-serializer')],
 };
 
-const expectedApiVersion = '51.0';
+const expectedApiVersion = '52.0';
 
 module.exports = { jestConfig, expectedApiVersion };
