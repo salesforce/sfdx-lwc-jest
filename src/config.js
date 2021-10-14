9d2a723e6d48d354a0dddcabf827dbc71c396651
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2021-10-15 01:51:24 +0530

    chore: update API version

--- src/config.js
+++ src/config.js
@@ -35,6 +35,6 @@ const jestConfig = {
     snapshotSerializers: [require.resolve('@lwc/jest-serializer')],
 };
 
-const expectedApiVersion = '52.0';
+const expectedApiVersion = '53.0';
 
 module.exports = { jestConfig, expectedApiVersion };
