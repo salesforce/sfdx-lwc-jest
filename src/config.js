f41619a7056ce03787a0047e726ec6267752aff3
Mohammed Abdul Sattar <abdulsattarmohammed@salesforce.com>
2022-08-06 11:20:30 +0530

    chore: release winter23

--- src/config.js
+++ src/config.js
@@ -35,6 +35,6 @@ const jestConfig = {
     snapshotSerializers: [require.resolve('@lwc/jest-serializer')],
 };
 
-const expectedApiVersion = '55.0';
+const expectedApiVersion = '56.0';
 
 module.exports = { jestConfig, expectedApiVersion };
