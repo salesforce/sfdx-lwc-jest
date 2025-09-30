068b8466a61bc69e7ba24083ec82088a617d7985
Will Harney <wharney@salesforce.com>
2025-09-30 17:05:56 -0400

    chore: replace license header script with eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
 import globals from 'globals';
+import header from 'eslint-plugin-header';
 import babelParser from '@babel/eslint-parser';
 
 export default defineConfig([
@@ -11,6 +12,27 @@ export default defineConfig([
                 ...globals.node,
                 ...globals.jest,
             },
+        },
+        plugins: { header },
+        rules: {
+            'header/header': [
+                'error',
+                'block',
+                [
+                    '',
+                    {
+                        pattern:
+                            '^ \\* Copyright \\(c\\) \\d{4}, ([sS]alesforce.com, inc|Salesforce, Inc)\\.$',
+                        // This copyright text should match the text used in the rollup config
+                        template: ` * Copyright (c) ${new Date().getFullYear()}, Salesforce, Inc.`,
+                    },
+                    ' * All rights reserved.',
+                    ' * SPDX-License-Identifier: MIT',
+                    ' * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT',
+                    ' ',
+                ],
+                1 /* newline after header */,
+            ],
         },
     },
     {
