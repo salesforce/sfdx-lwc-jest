ff918217b43aeff44599710be81d4e7ea6c9d25b
James Simone <16430727+jamessimone@users.noreply.github.com>
2025-10-15 14:18:50 -0400

    Adds Toast.show() static mock method for lightning/toast module

---
+++ src/lightning-stubs/toast/toast.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+export default class Toast {
+    static show = jest.fn();
+}
