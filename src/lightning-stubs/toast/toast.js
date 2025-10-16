6c404ffa45d1db0ee9a62ab24cc90b23e228cbe2
James Simone <16430727+jamessimone@users.noreply.github.com>
2025-10-15 21:23:29 -0400

    Update src/lightning-stubs/toast/toast.js
    Co-authored-by: Eugene Kashida <ekashida@gmail.com>

--- src/lightning-stubs/toast/toast.js
+++ src/lightning-stubs/toast/toast.js
@@ -5,5 +5,13 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 export default class Toast {
-    static show = jest.fn();
+    static show() {}
+    
+    @api labelLinks;
+    @api messageLinks;
+    @api label;
+    @api message;
+    @api variant;
+    @api mode;
+    @api focus() {}
 }
