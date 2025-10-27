78f24e7786369d42a86d5c9e8de076cc3ae576ae
Stephen Carraway <scarraway@salesforce.com>
2025-10-27 11:18:57 -0400

    update mock for lightning/graphql

--- src/lightning-stubs/graphql/graphql.js
+++ src/lightning-stubs/graphql/graphql.js
@@ -4,6 +4,34 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
-import { gql, graphql } from '../uiGraphQLApi/uiGraphQLApi';
+import { createTestWireAdapter } from '@salesforce/wire-service-jest-util';
+export class graphql extends createTestWireAdapter() {
+    static emit(value, filterFn, refreshFn) {
+        super.emit(
+            {
+                data: value,
+                errors: undefined,
+                ...(refreshFn !== undefined && { refresh: refreshFn }),
+            },
+            filterFn,
+        );
+    }
 
-export { gql, graphql };
+    static emitErrors(errors, filterFn, refreshFn) {
+        super.emit(
+            {
+                data: undefined,
+                errors,
+                ...(refreshFn !== undefined && { refresh: refreshFn }),
+            },
+            filterFn,
+        );
+    }
+
+    constructor(dataCallback) {
+        super(dataCallback);
+
+        graphql.emit(undefined);
+    }
+}
+export const gql = jest.fn();
